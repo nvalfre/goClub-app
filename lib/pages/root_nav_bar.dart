e52cb446ff25366e43d082856465799d7a488880
Nicolas Valfre <kokevalfre@gmail.com>
2019-11-07 17:19:50 -0300

    NV - add new route way with place holders for botton navigation bar, add drawers

---
+++ lib/pages/root_nav_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_go_club_app/pages/clubs_page.dart';
+import 'package:flutter_go_club_app/pages/home_user_page_backup.dart';
+
+import '../place_holder_widget.dart';
+import 'draw/draw_widget_user.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _childrenRoutes = [
+    PlaceholderWidget(HomePage()),
+    PlaceholderWidget(ClubsPage()),
+    PlaceholderWidget(ClubsPage()),
+    PlaceholderWidget(ClubsPage()),
+    PlaceholderWidget(ClubsPage())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _childrenRoutes[_currentIndex],
+      bottomNavigationBar: new Theme(
+        data: Theme.of(context).copyWith(
+            // sets the background color of the `BottomNavigationBar`
+            canvasColor: Colors.green,
+            // sets the active color of the `BottomNavigationBar` if `Brightness` is light
+            primaryColor: Colors.blue,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: new TextStyle(color: Colors.yellow))),
+        // sets the inactive color of the `BottomNavigationBar`
+
+        child: BottomNavigationBar(
+          onTap: onTabTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.collections_bookmark),
+              title: Text('Services'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text('Search')),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.class_),
+              title: Text('Classes'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.collections), title: Text('Clubs'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
